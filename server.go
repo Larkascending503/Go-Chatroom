e65fe7000c256768e7c63611092eba2d9368b14c
Bowen_Zheng <zbw1469902638@gmail.com>
2026-05-18 20:35:48 +0800

    add user management and message broadcasting functionality

--- server.go
+++ server.go
@@ -3,23 +3,60 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	// Online Map
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// Broadcast chan
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+func (this *Server) Broadcast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		this.mapLock.RLock()
+		for _, user := range this.OnlineMap {
+			user.C <- msg
+		}
+		this.mapLock.RUnlock()
+	}
+}
+
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("Connection established")
+	// fmt.Println("Connection established")
+	user := NewUser(conn)
+
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	this.Broadcast(user, "login")
+
+	select {}
 }
 
 // Start server
@@ -35,6 +72,7 @@ func (this *Server) Start() {
 	defer listener.Close()
 
 	// 2. accept
+	go this.ListenMessage()
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
