67e31f2dbc5c5c91f4a79aa7fc2a64691bcb4109
Larkascending503 <zbw1469902638@gmail.com>
2026-05-19 20:35:33 +0800

    add client.go and change the project structure

---
+++ client/client.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	conn       net.Conn
+}
+
+func NewClient(ServerIp string, ServerPort int) *Client {
+	client := &Client{
+		ServerIp:   ServerIp,
+		ServerPort: ServerPort,
+	}
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ServerIp, ServerPort))
+	if err != nil {
+		fmt.Println("net.Dial err:", err)
+		return nil
+	}
+
+	client.conn = conn
+
+	return client
+}
+
+func main() {
+	client := NewClient("127.0.0.1", 8888)
+
+}
